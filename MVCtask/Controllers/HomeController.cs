2dfd355e904f9cfbcaa290e2a51fc679b83e3236
zeyneb <zeynabej-bpa101@gmail.code.edu.az>
2026-04-28 06:22:44 +0400

    Add project files.

---
+++ MVCtask/Controllers/HomeController.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MVC_2.Models;
+using MVCtask.Models;
+
+namespace MVC_2.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            List<Product> products = new List<Product >
+{
+                new Product { Id = 1, Name = "Macallan 18 Year Old Sherry Oak", Price = 399.99m, ImageUrl = " kind-1.jpg ", Category = "Whiskey" },
+                new Product { Id = 2, Name = "Johnnie Walker Blue Label", Price = 249.99m, ImageUrl = " kind-2.jpg  ", Category = "Whiskey" },
+                new Product { Id = 3, Name = "Grey Goose VX Vodka", Price = 89.99m, ImageUrl = " kind-6.jpg  ", Category = "Vodka" },
+                new Product { Id = 4, Name = "Beluga Gold Line Vodka", Price = 129.99m, ImageUrl = " kind-4.jpg ", Category = "Vodka" },
+                new Product { Id = 5, Name = "Zacapa XO Rum", Price = 119.99m, ImageUrl = " kind-3.jpg ", Category = "Rum" },
+                new Product { Id = 6, Name = "Diplomatico Ambassador Rum", Price = 289.99m, ImageUrl = " kind-5.jpgv ", Category = "Rum" },
+    
+};
+          
+
+            List<Slider> sliders = new List<Slider>
+            {
+                new Slider
+                {
+                    Id = 1,
+                    Title = "Leave the page",
+                    Discount = 5,
+                    Desc = "Don’t drink",
+                    ImageUrl = "bg_2.jpg"
+                },
+
+            };
+
+            Home_VM vM = new Home_VM()
+            {
+                Products = products,
+                Sliders = sliders
+            };
+            return View(vM);
+
+        }
+    }
+}
---
+++ MVCtask/Models/ViewModels/HomeVM.cs
@@ -0,0 +1,11 @@
+﻿using MVCtask.Models;
+
+namespace MVC_2.Models
+{
+    public class Home_VM
+    {
+
+        public List<Product> Products { get; set; }
+        public List<Slider> Sliders { get; set; }
+    }
+}
